d191cca335539083882b4ee8b9ecda4c8c51195d
jonathanmrobbins <jonathanmrobbins@penguin.lxd>
2020-04-26 09:59:25 +0100

    dropped default file and added character creation

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,17 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+    <app-character-creation></app-character-creation>
   </div>
 </template>
 
 <script lang="ts">
 import Vue from "vue";
-import HelloWorld from "./components/HelloWorld.vue";
+import CharacterCreation from "./components/CharacterCreation/CharacterCreation.vue";
 
 export default Vue.extend({
   name: "App",
   components: {
-    HelloWorld
+    appCharacterCreation: CharacterCreation,
   }
 });
 </script>
